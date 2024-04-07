4c55923913cf3e710ab2e5b06303c372c075d897
talalhairizarham <157958881+talalhairizarham@users.noreply.github.com>
2024-04-07 00:43:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
